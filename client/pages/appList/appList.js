277e5d54749165065672cdffd84c9ba09dace39b
yanyi <yanyi609@126.com>
2018-04-24 20:10:01 +0800

    v2.0

---
+++ client/pages/appList/appList.js
@@ -0,0 +1,103 @@
+// pages/appList/appList.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    list: [
+      {
+        title: '闹钟',
+        bg: '#df6e66',
+        state: 1,
+        version: 1
+      }, {
+        title: '闹钟',
+        bg: '#ba6162',
+        state: 1,
+        version: 1
+      }, {
+        title: '闹钟',
+        bg: '#516161',
+        state: 1,
+        version: 1
+      }, {
+        title: '闹钟',
+        bg: '#798d75',
+        state: 1,
+        version: 1
+      }, {
+        title: '闹钟',
+        bg: '#bdb88d',
+        state: 1,
+        version: 1
+      }, {
+        title: '闹钟',
+        bg: '#df6e66',
+        state: 1,
+        version: 1
+      }, {
+        title: '闹钟',
+        bg: '#c88d77',
+        state: 1,
+        version: 1
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
